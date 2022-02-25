6b1c67303331479a72aef57162d80d99f3933243
Aadroman <100210701+Aadroman@users.noreply.github.com>
2022-02-25 12:27:53 +0100

    Add files via upload

---
+++ src/main/java/HelloJava.java
@@ -0,0 +1,14 @@
+/**
+ * Ceci est une classe de test qui ne fait pas grand chose
+ * @author JMB
+ * @author LC
+ */
+class HelloJava {
+	public static void afficherCancan(){
+		System.out.println("Et là qui voilà inspecteur gadget");
+	}
+
+	public static void main(String[] args) {
+		afficherCancan();
+	}
+}
